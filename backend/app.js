80d7032dd9dc84c348db6bc021716204370bb58e
Adrien <adrienquijo@gmail.com>
2025-02-18 16:17:12 +0100

    Ajout du routing des users, books et du static images -

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,31 @@
-import { Router } from 'express';
+import express from 'express';
+import { run } from './functions/api.js';
+import booksRoutes from './routes/books.routes.js';
+import userRoutes from './routes/users.routes.js';
+import path from 'path';
 
-Router;
+const __dirname = path.resolve();
+const app = express();
+
+run();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    );
+    next();
+});
+
+app.use('/api/books', booksRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+export default app;
