76228496bee99006f7c8d356f58b0c72a569fa43
Adrien <adrienquijo@gmail.com>
2025-02-18 16:25:11 +0100

    Ajout de la gestion des "authorization" en utilisant le token utilisateur -

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,24 @@
+import JsonWebToken from 'jsonwebtoken';
+
+/**
+ * Vérifie que l'utilisateur est bien connecté
+ * et que le Token n'a pas été altéré.
+ */
+export const auth = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = JsonWebToken.verify(
+            token,
+            process.env.TOKEN_SECRET
+        );
+        const userId = decodedToken.userId;
+
+        req.auth = {
+            userId: userId,
+        };
+
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'You are not authorized' });
+    }
+};
