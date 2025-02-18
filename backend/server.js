224f2aaa9488cd91d0674782497c9fab47cd12c3
Adrien <adrienquijo@gmail.com>
2025-02-18 16:18:18 +0100

    Ajout de l'utilisation du .ENV -

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-import { createServer } from "http";
-import app, { set } from "./app";
+import { createServer } from 'http';
+import app from './app.js';
+import 'dotenv/config.js';
 
 const normalizePort = (val) => {
     const port = parseInt(val, 10);
@@ -12,23 +13,25 @@ const normalizePort = (val) => {
     }
     return false;
 };
-const port = normalizePort(process.env.PORT || "3000");
-set("port", port);
+
+const port = normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
 
 const errorHandler = (error) => {
-    if (error.syscall !== "listen") {
+    if (error.syscall !== 'listen') {
         throw error;
     }
     const address = server.address();
     const bind =
-        typeof address === "string" ? "pipe " + address : "port: " + port;
+        typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
     switch (error.code) {
-        case "EACCES":
-            console.error(bind + " requires elevated privileges.");
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
             process.exit(1);
             break;
-        case "EADDRINUSE":
-            console.error(bind + " is already in use.");
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
             process.exit(1);
             break;
         default:
@@ -38,12 +41,12 @@ const errorHandler = (error) => {
 
 const server = createServer(app);
 
-server.on("error", errorHandler);
-server.on("listening", () => {
+server.on('error', errorHandler);
+server.on('listening', () => {
     const address = server.address();
     const bind =
-        typeof address === "string" ? "pipe " + address : "port " + port;
-    console.log("Listening on " + bind);
+        typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
 });
 
 server.listen(port);
