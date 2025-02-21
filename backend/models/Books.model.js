6f1071f83cd053e8f0176354aebddf7cba35d63c
Adrien <adrienquijo@gmail.com>
2025-02-21 12:23:46 +0100

    Modification du Schema pour inclure un chiffre minimum et maximum pour la note du Book - Ajout d'une fonction "pre" save qui permet de vérifier si un utilisateur a déjà noté ce book avant de le réinsérer dans la DB - Petit fix sur le Schema qui attendait un Strig à la place d'un int pour l'année de création - Ajout de JSDoc. Il est maintenant possible de récupérer les infos du Schema en important une doc @type nommée Book -

--- backend/models/Books.model.js
+++ backend/models/Books.model.js
@@ -1,19 +1,58 @@
 import mongoose from 'mongoose';
 
+/**
+ * @typedef {Object} Book
+ * @property {string} _id - Index de l'objet créé automatiquement par MongoDB
+ * @property {string} userId
+ * @property {string} title
+ * @property {string} author
+ * @property {string} imageUrl - Path de l'image sur le server
+ * @property {Number} year
+ * @property {string} genre
+ * @property {[object]} ratings - Un array contenant les différentes
+ * notes des utilisateurs sous forme d'objet
+ * @property {Number} averageRating
+ */
 const bookSchema = mongoose.Schema({
-    userId: { type: String, required: true, allowNull: false },
-    title: { type: String, required: true, allowNull: false },
-    author: { type: String, required: true, allowNull: false },
-    imageUrl: { type: String, required: true, allowNull: false },
-    year: { type: String, required: true, allowNull: false },
-    genre: { type: String, required: false, allowNull: true },
+    userId: { type: String, required: true },
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    year: { type: Number, required: true },
+    genre: { type: String, required: false },
     ratings: [
         {
             userId: { type: String },
-            grade: { type: Number },
+            grade: {
+                type: Number,
+                min: [1, 'La notation ne peux pas être inférieure à 1'],
+                max: [5, 'La notation ne peux pas être supérieure à 5'],
+            },
         },
     ],
     averageRating: { type: Number },
 });
 
+/**
+ * Vérifie que l'utilisateur n'ait pas déjà
+ * noté un livre -
+ */
+bookSchema.pre('save', function (next) {
+    const ratingUserId = [];
+    try {
+        for (const rating of this.ratings) {
+            if (ratingUserId.includes(rating.userId)) {
+                throw new Error(
+                    'Vous ne pouvez pas noter un livre plusieures fois',
+                    { cause: { status: 403 } }
+                );
+            } else {
+                ratingUserId.push(rating.userId);
+            }
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
 export default mongoose.model('Book', bookSchema);
