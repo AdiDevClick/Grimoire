9f08698c47ee6fb32ba33dceb27a41c49bf78a4a
Adrien <adrienquijo@gmail.com>
2025-02-21 12:29:46 +0100

    Ajout d'un "pre" save pour le schema qui va automatiquement hash le mot de passe avant la sauvegarde dans la DB - Ajout de la JS Doc pour le schema qui pourra être récupérer avec le @type User - Ajout d'un validateur dans le schema Mongoose vérifiera par une regex s'il utilise bien un email et s'il utilise un strong password -

--- backend/models/Users.model.js
+++ backend/models/Users.model.js
@@ -1,9 +1,55 @@
 import mongoose from 'mongoose';
 import mongooseUniqueValidator from 'mongoose-unique-validator';
+import bcrypt from 'bcrypt';
+import { emailRegex, passwordRegex } from '../configs/config.js';
 
-const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true, allowNull: false },
-    password: { type: String, required: true, allowNull: false },
+/**
+ * @typedef {Object} User
+ * @property {string} _id - Index de l'objet créé automatiquement par MongoDB
+ * @property {string} email - Email de l'utilisateur.
+ * @property {string} password - Mot de passe de l'utilisateur
+ */
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'Un email est requis'],
+        validate: {
+            validator: (v) => {
+                return emailRegex.test(v);
+            },
+            message: 'Un email est requis',
+        },
+    },
+    password: {
+        type: String,
+        required: [true, 'Un mot de passe est requis'],
+        validate: {
+            validator: (v) => {
+                return passwordRegex.test(v);
+            },
+            message:
+                'Le mot de passe doit contenir au moins 8 caractères, une lettre majuscule, une lettre minuscule, un chiffre, un caractère spécial',
+        },
+    },
+});
+
+/**
+ * Hash le mot de passe avant de sauvegarder l'utilisateur
+ */
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return console.log('not modified');
+        return next();
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
 });
 
 userSchema.plugin(mongooseUniqueValidator);
