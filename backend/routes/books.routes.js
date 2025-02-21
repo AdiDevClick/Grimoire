7d2be607100efc63e97a9be4a4f3ff51a3fe45c8
Adrien <adrienquijo@gmail.com>
2025-02-21 12:04:18 +0100

    Code fix : Suppression de l'utilisation de Multer dans certaines routes qui n'en avaient pas besoin -

--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -10,13 +10,14 @@ import {
 } from '../controllers/Book.controller.js';
 import { auth } from '../middlewares/auth.js';
 import multerConfig from '../middlewares/multer-config.js';
+// import multerConfig from '../middlewares/uploadImageMiddleware.js';
 
 const router = Router();
 
 router.post('/', auth, multerConfig, createBook);
-router.post('/:id/rating ', auth, multerConfig, rateOneBook);
+router.post('/:id/rating', auth, rateOneBook);
 router.put('/:id', auth, multerConfig, modifyBook);
-router.delete('/:id', auth, multerConfig, deleteBook);
+router.delete('/:id', auth, deleteBook);
 router.get('/', getAllBooks);
 router.get('/bestrating', getBestRatedBooks);
 router.get('/:id', getOneBook);
