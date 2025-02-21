761122cbb8dc72943c4321194a340e8da4b112f4
Adrien <adrienquijo@gmail.com>
2025-02-21 12:11:08 +0100

    Ajout de configurations pour la validation mail et password

--- backend/configs/config.js
+++ backend/configs/config.js
@@ -1,8 +1,19 @@
 /**
- * Spéficie les fichier à accepter ici
+ * Spéficier les fichier à accepter ici
  */
 export const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png',
 };
+
+/**
+ * Email validator
+ */
+export const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+
+/**
+ * Password validator
+ */
+export const passwordRegex =
+    /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*W)(?!.*s).{8,16}$/;
