d0d0d1ed0e3af6e3ca093edcc82312969af70efe
Adrien <adrienquijo@gmail.com>
2025-02-22 11:42:44 +0100

    Code Cleanup

--- backend/routes/books.routes.js
+++ backend/routes/books.routes.js
@@ -10,7 +10,6 @@ import {
 } from '../controllers/Book.controller.js';
 import { auth } from '../middlewares/auth.js';
 import multerConfig from '../middlewares/multer-config.js';
-// import multerConfig from '../middlewares/uploadImageMiddleware.js';
 
 const router = Router();
 
