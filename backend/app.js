29ecb2b94b246d77a3772e1e974e716331744c78
Adrien <adrienquijo@gmail.com>
2025-03-13 11:07:10 +0100

    Ajout d'un préchargement du fichier .env en fonction de la configuration de l'environnement

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,16 @@ import { run } from './functions/api.js';
 import booksRoutes from './routes/books.routes.js';
 import userRoutes from './routes/users.routes.js';
 import path from 'path';
+import loadEnv from './configs/loadEnv.config.js';
+import { tooBusyHandler } from './middlewares/tooBusy.js';
 
 const __dirname = path.resolve();
 const app = express();
 
+// Sets ENV variables
+loadEnv();
+
+// Connects to MongoDB
 run();
 
 app.use(express.json());
@@ -21,12 +27,11 @@ app.use((req, res, next) => {
         'Access-Control-Allow-Methods',
         'GET, POST, PUT, DELETE, PATCH, OPTIONS'
     );
-
     next();
 });
 
-app.use('/api/books', booksRoutes);
-app.use('/api/auth', userRoutes);
+app.use('/api/books', tooBusyHandler, booksRoutes);
+app.use('/api/auth', tooBusyHandler, userRoutes);
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 export default app;
